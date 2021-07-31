5d04558523d816e7a1606644e04adf4d073d45bd
Austin Summerlin <summerlin.austin@gmail.com>
2021-07-30 18:08:01 -0700

    add ci intergration and fix tests

--- src/components/ArticlesList.jsx
+++ src/components/ArticlesList.jsx
@@ -1,42 +1,40 @@
+/* eslint-disable keyword-spacing */
 import React from 'react';
 import PropTypes from 'prop-types';
 import Article from './Article';
 
 const ArticlesList = ({ articles }) => {
-  return (
-    <ul aria-label="articles">
-      {articles.map(article => {
-        return (
-          <li
-            key={article.url}
-            style={{
-              listStyle: 'none',
-              border: '1px solid pink',
-              borderRadius: '5px',
-              margin: '2px',
-              width: '50%',
-              backgroundColor: 'lightpink'
-            }}
-          >
-            <Article article={article} />
-          </li>
-        );
-      })}
-    </ul>
-  );
-}
+  if (articles) {
+    const articleElements = articles.map(article => (
+      <li
+        key={article.title}
+        style={{
+          listStyle: 'none',
+          fontSize: '.7em',
+          border: '2px solid black',
+          borderRadius: '5px',
+          margin: '2px',
+          backgroundColor: 'lightpink'
+        }}>
+        <Article
+          title={article.title}
+          author={article.author}
+          url={article.url}
+        />
+      </li>
+    ));
+    return <ul>{articleElements}</ul>;
+  } return <ul>Oops...something went wrong</ul>;
+};
 
 ArticlesList.propTypes = {
   articles: PropTypes.arrayOf(
     PropTypes.shape({
-      title: PropTypes.string.isRequired,
-      author: PropTypes.string.isRequired,
-      url: PropTypes.string.isRequired,
+      title: PropTypes.stringisRequired,
+      author: PropTypes.string,
+      url: PropTypes.string
     })
-  ).isRequired
+  ),
 };
 
 export default ArticlesList;
-
-
-
--- src/services/NewsApi.js
+++ src/services/NewsApi.js
@@ -1,25 +1,19 @@
 export const fetchNews = async () => {
 
   const res = await fetch(`https://newsapi.org/v2/everything?q=news&pageSize=5&apiKey=${process.env.REACT_APP_API_KEY}`);
-  const { articles } = await res.json();
+  const json = await res.json();
 
-  return articles.map(article => ({
-    title: article.title,
-    author: article.author,
-    url: article.url,
-  }));
+  return json.articles;
 };
+
+
 
 export async function SearchArticles(searchInput) {
 
   const res = await fetch(`https://newsapi.org/v2/everything?q=${searchInput}&pageSize=5&apiKey=${process.env.REACT_APP_API_KEY}`);
-  const { articles } = await res.json();
+  const json = await res.json();
 
-  return articles.map(article => ({
-    title: article.title,
-    author: article.author,
-    url: article.url,
-  }));
+  return json.articles;
 }
 
 
